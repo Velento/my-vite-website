7596851b4d4d02d9ab6da37d255feabe2ff37364
Velento_Andrei <goggopizza.velento@gmail.com>
2024-06-20 22:59:09 +0300

    Updated project settings for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/my-vite-website/', // Укажите базовый путь до корня вашего проекта
+  build: {
+    rollupOptions: {
+      input: '/public/index.html', // Путь к index.html
+    }
+  }
+});
