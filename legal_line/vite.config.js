fb4b50156b6e38a8c734bcda10e01fe3dbe2f3c2
Velento_Andrei <goggopizza.velento@gmail.com>
2024-06-21 17:09:26 +0300

    Update deployment settings #2

--- legal_line/vite.config.js
+++ legal_line/vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,7 +6,7 @@ export default defineConfig({
   base: '/legal_line/', // Укажите базовый путь до корня вашего проекта
   build: {
     rollupOptions: {
-      input: './index.html' // Путь к index.html в корне проекта
+      input: './legal_line/index.html' // Путь к index.html в корне папки legal_line
     }
   }
 });
