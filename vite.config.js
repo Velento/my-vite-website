16cd6139ebf5fa7aeb49a985e9cb51d18a0f40d4
Jarek <jareksavchenko@gmail.com>
2026-04-06 15:24:15 +0200

    fix: change vite base path to root for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/legal_line/',
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
